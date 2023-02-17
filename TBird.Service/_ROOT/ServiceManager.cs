71ad3f9a39817dd1d88bc796ccb8bb62a8a43d21
horie <horie@able-biott.co.jp>
2023-02-17 17:30:44 +0900

    Windowsサービス実行用ライブラリの作成

---
+++ TBird.Service/_ROOT/ServiceManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using TBird.Core;
+
+namespace TBird.Service
+{
+    public abstract class ServiceManager : ServiceBase, ILocker
+    {
+        public ServiceManager(Func<Task> tick, Func<bool> start, Action stop) : this (tick, () => start == null ? null : CoreUtil.WaitAsync(start), stop)
+        {
+
+        }
+
+        public ServiceManager(Func<Task> tick, Func<Task<bool>> start, Action stop)
+        {
+            Lock = this.CreateLock4Instance();
+
+            //自動ﾛｸﾞ採取を有効
+            AutoLog = true;
+
+            // ｻｰﾋﾞｽの終了、停止可能
+            CanShutdown = true;
+            CanStop = true;
+
+            // このｻｰﾋﾞｽの名前
+            ServiceName = ServiceSetting.Instance.ServiceName;
+
+            ServiceFactory.MessageService = new ServiceMessageService(EventLog);
+            ServiceFactory.MessageService.Info("コンストラクタが呼び出されました。");
+
+            _startfunc = start;
+            _tickfunc = tick;
+            _stop = stop;
+            _timer = new IntervalTimer(async () =>
+            {
+                using (await this.LockAsync())
+                {
+                    // 開始処理を行っていない場合は開始処理を行い、正常終了したら後続処理を行う。
+                    if (_startasync = _startasync || _startfunc == null || await _startfunc())
+                    {
+                        await _tickfunc();
+                    }
+                }
+            });
+            _timer.Interval = TimeSpan.FromMilliseconds(ServiceSetting.Instance.Interval);
+        }
+
+        private IntervalTimer _timer;
+
+        private Func<Task<bool>> _startfunc;
+
+        private Func<Task> _tickfunc;
+
+        private Action _stop;
+
+        private bool _startasync = false;
+
+        public string Lock { get; private set; }
+
+        /// <summary>
+        /// ｻｰﾋﾞｽを開始します。
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            ServiceFactory.MessageService.Info("テストサービスを開始します。");
+            _startasync = false;
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// ｻｰﾋﾞｽを停止します。
+        /// </summary>
+        protected override void OnStop()
+        {
+            _timer.Stop();
+
+            //お作法らしい
+            RequestAdditionalTime(2000);
+
+            ServiceFactory.MessageService.Info("テストサービスを停止します。");
+
+            if (_stop != null) _stop();
+
+            // 正常終了を通知
+            ExitCode = 0;
+        }
+
+        /// <summary>
+        /// ｼｽﾃﾑｼｬｯﾄﾀﾞｳﾝ時の処理を実行します。
+        /// </summary>
+        protected override void OnShutdown()
+        {
+            ServiceFactory.MessageService.Info("テストサービスがシステムの終了を検知しました。");
+            _timer.Dispose();
+        }
+    }
+}
---
+++ TBird.Service/_ROOT/ServiceMessageService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TBird.Core;
+
+namespace TBird.Service
+{
+    public class ServiceMessageService : ConsoleMessageService
+    {
+        public ServiceMessageService(EventLog log)
+        {
+            _log = log;
+        }
+        private EventLog _log;
+
+        public override void Debug(string message, [CallerMemberName] string callerMemberName = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            base.Debug(message, callerMemberName, callerFilePath, callerLineNumber);
+        }
+
+        public override void Info(string message, [CallerMemberName] string callerMemberName = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            base.Info(message, callerMemberName, callerFilePath, callerLineNumber);
+        }
+
+        public override void Error(string message, [CallerMemberName] string callerMemberName = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            base.Error(message, callerMemberName, callerFilePath, callerLineNumber);
+        }
+
+        public override void Exception(Exception exception, [CallerMemberName] string callerMemberName = "", [CallerFilePath] string callerFilePath = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            base.Exception(exception, callerMemberName, callerFilePath, callerLineNumber);
+        }
+    }
+}
