f93760d23ab038d7426fa1815eddda1234afea1c
horie <horie@able-biott.co.jp>
2023-02-17 17:32:01 +0900

    正確な間隔でタイマー実行するためのクラスを追加

---
+++ TBird.Core/_ROOT/IntervalTimer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TBird.Core
+{
+    public partial class IntervalTimer : ILocker
+    {
+        public IntervalTimer(Action action) : this(() => CoreUtil.WaitAsync(action))
+        {
+
+        }
+
+        public IntervalTimer(Func<Task> func)
+        {
+            Lock = this.CreateLock4Instance();
+
+            _func = func;
+            _timer = new Timer(Tick);
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        /// <summary>ｲﾝﾀｰﾊﾞﾙ処理</summary>
+        private Func<Task> _func;
+
+        /// <summary>内部ﾀｲﾏｰ</summary>
+        private Timer _timer;
+
+        /// <summary>処理開始時間</summary>
+        private DateTime _starttime;
+
+        private bool _isprocessing;
+
+        /// <summary>破棄時のｷｬﾝｾﾙﾄｰｸﾝ</summary>
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+
+        /// <summary>ﾛｯｸｷｰ</summary>
+        public string Lock { get; private set; }
+
+        /// <summary>処理間隔を設定、または取得します。</summary>
+        public TimeSpan Interval { get; set; }
+
+        /// <summary>
+        /// 一定間隔の処理を実行します。
+        /// </summary>
+        private async void Tick(object sender)
+        {
+            // 処理の重複禁止
+            using (await this.LockAsync())
+            {
+                // ﾀｲﾏｰ停止
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+
+                try
+                {
+                    // 非同期処理を実行
+                    await _func().Cts(_cts);
+                }
+                catch (TimeoutException)
+                {
+                    // ｷｬﾝｾﾙ時はｽｷｯﾌﾟ
+                }
+                catch (Exception ex)
+                {
+                    ServiceFactory.MessageService.Exception(ex);
+                }
+                finally
+                {
+                    if (_isprocessing && !disposedValue && !_cts.IsCancellationRequested)
+                    {
+                        // 処理時間を算出
+                        var milliseconds = (DateTime.Now - _starttime).TotalMilliseconds;
+                        // 処理開始時間を進めるｶｳﾝﾄを算出
+                        var nextcount = Math.Ceiling(milliseconds / Interval.TotalMilliseconds);
+                        // 処理開始時間を再計算
+                        _starttime += TimeSpan.FromMilliseconds(Interval.TotalMilliseconds * nextcount);
+                        // 次回開始時刻設定
+                        _timer.Change((int)(_starttime - DateTime.Now).TotalMilliseconds, Timeout.Infinite);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 一定間隔の処理を開始します。
+        /// </summary>
+        public void Start()
+        {
+            if (disposedValue) throw new ObjectDisposedException("This timer has been destroyed.");
+            // 処理開始ﾌﾗｸﾞ
+            _isprocessing = true;
+            // 処理開始時間設定
+            _starttime = DateTime.Now;
+            // ﾀｲﾏｰを1回だけ即時実行
+            _timer.Change(0, Timeout.Infinite);
+        }
+
+        /// <summary>
+        /// 一定間隔の処理を停止します。
+        /// </summary>
+        public void Stop()
+        {
+            if (disposedValue) throw new ObjectDisposedException("This timer has been destroyed.");
+            // 処理開始ﾌﾗｸﾞ
+            _isprocessing = false;
+            // ﾀｲﾏｰ停止
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+    }
+}
---
+++ TBird.Core/_ROOT/IntervalTimer_dispose.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBird.Core
+{
+    public partial class IntervalTimer : IDisposable
+    {
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Stop();
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                    _cts.Cancel();
+                    _timer.Dispose();
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        // ~IntervalTimer()
+        // {
+        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
