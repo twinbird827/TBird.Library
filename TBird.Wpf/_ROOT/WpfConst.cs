274da817c7c8e1c26a808385cf954165e15bbef8
horie <horie@able-biott.co.jp>
2023-05-19 18:47:52 +0900

    多言語化対応

---
+++ TBird.Wpf/_ROOT/WpfConst.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TBird.Core;
+
+namespace TBird.Wpf
+{
+    public static class WpfConst
+    {
+        /// <summary>OK</summary>
+        public static string L_Ok { get; } = Lang.Instance.Get();
+
+        /// <summary>ｷｬﾝｾﾙ</summary>
+        public static string L_Cancel { get; } = Lang.Instance.Get();
+    }
+}
