9bd1fe4087e82df8794d118bef496d07bed806df
able-khorie <horie@able-biott.co.jp>
2023-06-04 15:35:32 +0900

    エレメント値を各種プリミティブ型で取得するための関数を追加

--- TBird.Core/Extensions/XmlExtension.cs
+++ TBird.Core/Extensions/XmlExtension.cs
@@ -77,5 +77,77 @@ namespace TBird.Core
         {
             return EnumUtil.ToEnum<T>(xml.AttributeS(name));
         }
+
+        /// <summary>
+        /// Xmlｴﾚﾒﾝﾄをbool値に変換して取得します。
+        /// </summary>
+        /// <param name="xml"><see cref="XElement"/>ｲﾝｽﾀﾝｽ</param>
+        /// <param name="name">ｴﾚﾒﾝﾄの名前</param>
+        /// <returns></returns>
+        public static bool ElementB(this XElement xml, XName name, bool def = true)
+        {
+            var attr = xml.Element(name);
+            return attr != null ? (bool)attr : def;
+        }
+
+        /// <summary>
+        /// Xmlｴﾚﾒﾝﾄをstring値に変換して取得します。
+        /// </summary>
+        /// <param name="xml"><see cref="XElement"/>ｲﾝｽﾀﾝｽ</param>
+        /// <param name="name">ｴﾚﾒﾝﾄの名前</param>
+        /// <returns></returns>
+        public static string ElementS(this XElement xml, XName name, string def = null)
+        {
+            var attr = xml.Element(name);
+            return attr != null ? (string)attr : def;
+        }
+
+        /// <summary>
+        /// Xmlｴﾚﾒﾝﾄをint値に変換して取得します。
+        /// </summary>
+        /// <param name="xml"><see cref="XElement"/>ｲﾝｽﾀﾝｽ</param>
+        /// <param name="name">ｴﾚﾒﾝﾄの名前</param>
+        /// <returns></returns>
+        public static int ElementI(this XElement xml, XName name, int def = 0)
+        {
+            var attr = xml.Element(name);
+            return attr != null ? (int)attr : def;
+        }
+
+        /// <summary>
+        /// Xmlｴﾚﾒﾝﾄをlong値に変換して取得します。
+        /// </summary>
+        /// <param name="xml"><see cref="XElement"/>ｲﾝｽﾀﾝｽ</param>
+        /// <param name="name">ｴﾚﾒﾝﾄの名前</param>
+        /// <returns></returns>
+        public static long ElementL(this XElement xml, XName name, long def = 0)
+        {
+            var attr = xml.Element(name);
+            return attr != null ? (long)attr : def;
+        }
+
+        /// <summary>
+        /// Xmlｴﾚﾒﾝﾄをlong値に変換して取得します。
+        /// </summary>
+        /// <param name="xml"><see cref="XElement"/>ｲﾝｽﾀﾝｽ</param>
+        /// <param name="name">ｴﾚﾒﾝﾄの名前</param>
+        /// <returns></returns>
+        public static double ElementD(this XElement xml, XName name, double def = 0)
+        {
+            var attr = xml.Element(name);
+            return attr != null ? (double)attr : def;
+        }
+
+        /// <summary>
+        /// Xmlｴﾚﾒﾝﾄを列挙値に変換して取得します。
+        /// </summary>
+        /// <param name="xml"><see cref="XElement"/>ｲﾝｽﾀﾝｽ</param>
+        /// <param name="name">ｴﾚﾒﾝﾄの名前</param>
+        /// <returns></returns>
+        public static T ElementE<T>(this XElement xml, XName name) where T : struct
+        {
+            return EnumUtil.ToEnum<T>(xml.ElementS(name));
+        }
+
     }
 }
