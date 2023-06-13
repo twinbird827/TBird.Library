854f24a2972afc4b5a272eb6878bd9d029515646
horie <horie@able-biott.co.jp>
2023-06-13 19:56:08 +0900

    initial_0613

---
+++ TBird.Wpf/Behaviors/FrameworkElementBehavior_Size.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TBird.Wpf.Behaviors
+{
+    public partial class FrameworkElementBehavior
+    {
+        public static DependencyProperty WidthProperty = BehaviorUtil.RegisterAttached(
+            "Width", typeof(FrameworkElementBehavior), default(double), OnSetWidthCallback
+        );
+
+        public static void SetWidth(DependencyObject target, object value)
+        {
+            target.SetValue(WidthProperty, value);
+        }
+
+        public static double GetWidth(DependencyObject target)
+        {
+            return (double)target.GetValue(WidthProperty);
+        }
+
+        private static void OnSetWidthCallback(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            if (target is FrameworkElement fe && e.NewValue is double width && !double.IsNaN(width) && 0 < width)
+            {
+                fe.Width = width;
+                fe.MaxWidth = width;
+                fe.MinWidth = width;
+            }
+        }
+
+        public static DependencyProperty HeightProperty = BehaviorUtil.RegisterAttached(
+            "Height", typeof(FrameworkElementBehavior), default(double), OnSetHeightCallback
+        );
+
+        public static void SetHeight(DependencyObject target, object value)
+        {
+            target.SetValue(HeightProperty, value);
+        }
+
+        public static double GetHeight(DependencyObject target)
+        {
+            return (double)target.GetValue(HeightProperty);
+        }
+
+        private static void OnSetHeightCallback(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            if (target is FrameworkElement fe && e.NewValue is double height && !double.IsNaN(height) && 0 < height)
+            {
+                fe.Height = height;
+                fe.MaxHeight = height;
+                fe.MinHeight = height;
+            }
+        }
+    }
+}
---
+++ TBird.Wpf/Converters/Enum2StringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TBird.Core;
+
+namespace TBird.Wpf.Converters
+{
+    public class Enum2StringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var tmp = parameter as Enum;
+            if (tmp != null)
+            {
+                return tmp.GetLabel();
+            }
+            else
+            {
+                return parameter;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
