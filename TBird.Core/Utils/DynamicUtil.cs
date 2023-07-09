d9f052fc114412d7085937b569af35618a4107db
able-khorie <96095182+able-khorie@users.noreply.github.com>
2023-07-10 00:22:47 +0900

    init0709

---
+++ TBird.Core/Utils/DynamicUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TBird.Core
+{
+    public static class DynamicUtil
+    {
+        public static int I(dynamic value)
+        {
+            return value is int i
+                ? i
+                : int.Parse(S(value));
+        }
+
+        public static long L(dynamic value)
+        {
+            return value is long i
+                ? i
+                : long.Parse(S(value));
+        }
+
+        public static string S(dynamic value)
+        {
+            return value is string s
+                ? s
+                : $"{value}";
+        }
+    }
+}
