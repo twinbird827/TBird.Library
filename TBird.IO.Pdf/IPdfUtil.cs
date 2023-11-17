ded4dfc7ae2f92c2eb20e5e9f94c96daa8f0628b
horie <horie@able-biott.co.jp>
2023-11-17 14:17:30 +0900

    ライブラリを実行ファイルに変更してGhostScriptの処理は別プロセスで実行するように修正

---
+++ TBird.IO.Pdf/IPdfUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBird.IO.Pdf
+{
+	internal interface IPdfUtil
+	{
+		/// <summary>
+		/// 指定したPDFのﾍﾟｰｼﾞ数を取得します。
+		/// </summary>
+		/// <param name="pdffile">PDFﾌｧｲﾙﾊﾟｽ</param>
+		/// <returns></returns>
+		int GetPageSize(string pdffile);
+
+		/// <summary>
+		/// 指定したPDFをﾍﾟｰｼﾞ毎に画像化します。
+		/// </summary>
+		/// <param name="pdffile">PDFﾌｧｲﾙﾊﾟｽ</param>
+		/// <param name="start">画像化する最初のﾍﾟｰｼﾞ番号</param>
+		/// <param name="end">画像化する最後のﾍﾟｰｼﾞ番号</param>
+		/// <param name="dpi">解像度</param>
+		void Pdf2Jpg(string pdffile, int start, int end, int dpi);
+
+		/// <summary>
+		/// PDFﾌｧｲﾙのﾌｯﾀにﾍﾟｰｼﾞ番号を追加します。
+		/// </summary>
+		/// <param name="pdffile">PDFﾌｧｲﾙﾊﾟｽ</param>
+		void PutPageNumber(string pdffile);
+	}
+}
