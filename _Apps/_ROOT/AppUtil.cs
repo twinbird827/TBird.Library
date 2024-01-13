8749b1489daab09bbb77c61579337f7723e1b02f
twinbird827 <35794567+twinbird827@users.noreply.github.com>
2024-01-14 08:17:01 +0900

    init2401140816

---
+++ _Apps/_ROOT/AppUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Netkeiba
+{
+	public static class AppUtil
+	{
+		public static string GetInnerHtml(this AngleSharp.Dom.IElement x)
+		{
+			var innerhtml = x.GetElementsByTagName("span").Any() 
+				? x.GetElementsByTagName("span").First().InnerHtml
+				: x.InnerHtml;
+			return Regex.Replace(innerhtml.Replace("&nbsp;", " "), " +", " ");
+		}
+
+		public static string GetHrefAttribute(this AngleSharp.Dom.IElement x, string attribute)
+		{
+			return $"{x.GetElementsByTagName("a").Select(a => a.GetAttribute(attribute)).First()}";
+		}
+
+		public static string GetTryCatch(this AngleSharp.Dom.IElement x, Func<string, string> func)
+		{
+			try
+			{
+				return func(x.GetInnerHtml());
+			}
+			catch
+			{
+				return string.Empty;
+			}
+		}
+	}
+}
---
+++ _Apps/_ROOT/ProgressViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TBird.Wpf;
+
+namespace Netkeiba._ROOT
+{
+	public class ProgressViewModel : BindableBase
+	{
+		public double Value
+		{
+			get => _Value;
+			set => SetProperty(ref _Value, value);
+		}
+		private double _Value;
+
+		public double Minimum
+		{
+			get => _Minimum;
+			set => SetProperty(ref _Minimum, value);
+		}
+		private double _Minimum;
+
+		public double Maximum
+		{
+			get => _Maximum;
+			set => SetProperty(ref _Maximum, value);
+		}
+		private double _Maximum;
+
+	}
+}
