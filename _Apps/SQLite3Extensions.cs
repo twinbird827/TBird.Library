c2bb6e54b811c76fb1ca36485c5fa62edcfa186c
horie <horie@able-biott.co.jp>
2025-07-30 18:10:15 +0900

    途中のデータ

--- _Apps/SQLite3Extensions.cs
+++ _Apps/SQLite3Extensions.cs
@@ -9,7 +9,7 @@ using TBird.DB.SQLite;
 
 namespace Netkeiba
 {
-	public static class SQLite3Extensions
+	public static partial class SQLite3Extensions
 	{
 		private static string[] Arr(params string[] arr) => arr;
 
@@ -27,113 +27,5 @@ namespace Netkeiba
 			await conn.ExecuteNonQueryAsync(sql, prm);
 		}
 
-		/// <summary>
-		/// 教育ﾃﾞｰﾀ作成用ﾃｰﾌﾞﾙを削除します。
-		/// </summary>
-		/// <param name="conn"></param>
-		/// <returns></returns>
-		public static async Task DropAllTablesAsync(this SQLiteControl conn)
-		{
-			await conn.ExecuteNonQueryAsync("DROP TABLE IF EXISTS t_orig");
-			await conn.ExecuteNonQueryAsync("DROP TABLE IF EXISTS t_shutuba");
-			await conn.ExecuteNonQueryAsync("DROP TABLE IF EXISTS t_model");
-		}
-
-		/// <summary>
-		/// 確定前のﾃﾞｰﾀを削除します。
-		/// </summary>
-		/// <param name="conn"></param>
-		/// <returns></returns>
-		public static async Task RemoveShortageMissingDatasAsync(this SQLiteControl conn)
-		{
-			if (await conn.ExistsColumn("t_orig", "ﾚｰｽID"))
-			{
-				await conn.BeginTransaction();
-				await conn.ExecuteNonQueryAsync("DELETE FROM t_orig WHERE 着順 IS NULL");
-				await conn.ExecuteNonQueryAsync("DELETE FROM t_orig WHERE 着順 = ''");
-				await conn.ExecuteNonQueryAsync("DELETE FROM t_orig WHERE 着順 = 0");
-				conn.Commit();
-			}
-		}
-
-		/// <summary>
-		/// 最後に確定ﾃﾞｰﾀを取得した月を取得します。
-		/// </summary>
-		/// <param name="conn"></param>
-		/// <returns></returns>
-		public static async Task<DateTime> GetLastMonth(this SQLiteControl conn)
-		{
-			var datestring = await conn.ExistsColumn("t_orig_h", "ﾚｰｽID")
-				? await conn.ExecuteScalarAsync("SELECT IFNULL(MAX(開催日), '2010/01/01') FROM t_orig_h").RunAsync(x => x.Str())
-				: "2010/01/01";
-			return DateTime.Parse(datestring);
-		}
-
-		/// <summary>馬ﾍｯﾀﾞ</summary>
-		private static readonly string[] col_orig_h = Arr("ﾚｰｽID", "ﾚｰｽ名", "開催日", "開催日数", "開催場所", "ﾗﾝｸ1", "ﾗﾝｸ2", "回り", "距離", "天候", "馬場", "馬場状態");
-
-		/// <summary>馬明細</summary>
-		private static readonly string[] col_orig_d = Arr("ﾚｰｽID", "着順", "枠番", "馬番", "馬名", "馬ID", "馬性", "馬齢", "斤量", "騎手名", "騎手ID", "ﾀｲﾑ", "ﾀｲﾑ変換", "着差", "ﾀｲﾑ指数", "通過", "上り", "単勝", "人気", "体重", "増減", "備考", "調教場所", "調教師名", "調教師ID", "馬主名", "馬主ID", "賞金");
-
-		public static async Task CreateOrig(this SQLiteControl conn)
-		{
-			await conn.Create(
-				"t_orig_h",
-				col_orig_h,
-				Arr("ﾚｰｽID")
-			);
-
-			// TODO 開催日時と着順をINTEGERにする
-
-			await conn.Create(
-				"t_orig_d",
-				col_orig_d,
-				Arr("ﾚｰｽID", "馬番")
-			);
-
-			await conn.Create(
-				"t_orig_k",
-				Arr("馬ID", "父ID", "母ID"),
-				Arr("馬ID")
-			);
-
-			// TODO indexの作成
-		}
-
-		public static async Task InsertOrigAsync(this SQLiteControl conn, List<Dictionary<string, string>> racearr)
-		{
-			async Task InsertKettoAsync(string uma)
-			{
-				if (0 == await conn.ExecuteScalarAsync("SELECT COUNT(*) FROM t_orig_k WHERE 馬ID = ?", SQLiteUtil.CreateParameter(DbType.Object, uma)).RunAsync(x => x.GetInt32()))
-				{
-					await foreach (var ketto in NetkeibaGetter.GetKetto(uma))
-					{
-						await conn.InsertAsync("t_orig_k", ketto);
-					}
-				}
-			}
-
-			var first = true;
-
-			foreach (var x in racearr)
-			{
-				if (first)
-				{
-					first = false;
-					await conn.InsertAsync("t_orig_h", col_orig_h.ToDictionary(s => s, s => x[s]));
-				}
-				await conn.InsertAsync("t_orig_d", col_orig_d.ToDictionary(s => s, s => x[s]));
-				await InsertKettoAsync(x["馬ID"]);
-			}
-		}
-
-		public static async Task<bool> ExistsOrigAsync(this SQLiteControl conn, string raceid)
-		{
-			var cnt = await conn.ExecuteScalarAsync(
-				"SELECT COUNT(*) FROM t_orig_h WHERE ﾚｰｽID = ?",
-				SQLiteUtil.CreateParameter(DbType.String, raceid)
-			).RunAsync(x => x.GetInt32());
-			return 0 < cnt;
-		}
 	}
 }
--- _Apps/STEP1Command.cs
+++ _Apps/STEP1Command.cs
@@ -30,7 +30,7 @@ namespace Netkeiba
 					DirectoryUtil.Create(Path.GetDirectoryName(AppUtil.Sqlitepath));
 
 					// 作成し直すために全ﾃｰﾌﾞﾙDROP
-					await conn.DropAllTablesAsync();
+					await conn.DropSTEP1();
 				}
 
 				// CREATE
@@ -87,6 +87,132 @@ namespace Netkeiba
 
 				yield return arr;
 			}
+		}
+	}
+
+	public static partial class SQLite3Extensions
+	{
+		/// <summary>
+		/// 教育ﾃﾞｰﾀ作成用ﾃｰﾌﾞﾙを削除します。
+		/// </summary>
+		/// <param name="conn"></param>
+		/// <returns></returns>
+		public static async Task DropSTEP1(this SQLiteControl conn)
+		{
+			await conn.ExecuteNonQueryAsync("DROP TABLE IF EXISTS t_orig_h");
+			await conn.ExecuteNonQueryAsync("DROP TABLE IF EXISTS t_orig_d");
+			await conn.ExecuteNonQueryAsync("DROP TABLE IF EXISTS t_orig_k");
+		}
+
+		/// <summary>
+		/// 確定前のﾃﾞｰﾀを削除します。
+		/// </summary>
+		/// <param name="conn"></param>
+		/// <returns></returns>
+		public static async Task RemoveShortageMissingDatasAsync(this SQLiteControl conn)
+		{
+			if (await conn.ExistsColumn("t_orig_d", "ﾚｰｽID"))
+			{
+				await conn.BeginTransaction();
+				await conn.ExecuteNonQueryAsync("DELETE FROM t_orig_d WHERE 着順 IS NULL");
+				await conn.ExecuteNonQueryAsync("DELETE FROM t_orig_d WHERE 着順 = ''");
+				await conn.ExecuteNonQueryAsync("DELETE FROM t_orig_d WHERE 着順 = 0");
+				conn.Commit();
+			}
+		}
+
+		/// <summary>
+		/// 最後に確定ﾃﾞｰﾀを取得した月を取得します。
+		/// </summary>
+		/// <param name="conn"></param>
+		/// <returns></returns>
+		public static async Task<DateTime> GetLastMonth(this SQLiteControl conn)
+		{
+			var datestring = await conn.ExistsColumn("t_orig_h", "ﾚｰｽID")
+				? await conn.ExecuteScalarAsync("SELECT IFNULL(MAX(開催日), '2010/01/01') FROM t_orig_h").RunAsync(x => x.Str())
+				: "2010/01/01";
+			return DateTime.Parse(datestring);
+		}
+
+		/// <summary>ﾚｰｽﾍｯﾀﾞ</summary>
+		private static readonly string[] col_orig_h = Arr("ﾚｰｽID", "ﾚｰｽ名", "開催日", "開催日数", "開催場所", "ﾗﾝｸ1", "ﾗﾝｸ2", "回り", "距離", "天候", "馬場", "馬場状態", "優勝賞金", "頭数");
+
+		/// <summary>ﾚｰｽ明細</summary>
+		private static readonly string[] col_orig_d = Arr("ﾚｰｽID", "着順", "枠番", "馬番", "馬名", "馬ID", "馬性", "馬齢", "斤量", "騎手名", "騎手ID", "ﾀｲﾑ", "ﾀｲﾑ変換", "着差", "ﾀｲﾑ指数", "通過", "上り", "単勝", "人気", "体重", "増減", "備考", "調教場所", "調教師名", "調教師ID", "馬主名", "馬主ID", "賞金");
+
+		private static readonly string[] col_uma = Arr("馬ID", "誕生日", "購入額", "馬主ID", "父ID", "母父ID");
+
+		public static async Task CreateOrig(this SQLiteControl conn)
+		{
+			await conn.Create(
+				"t_orig_h",
+				col_orig_h,
+				Arr("ﾚｰｽID")
+			);
+
+			// TODO 開催日時と着順をINTEGERにする
+
+			await conn.Create(
+				"t_orig_d",
+				col_orig_d,
+				Arr("ﾚｰｽID", "馬番")
+			);
+
+			await conn.Create(
+				"t_uma",
+				col_uma,
+				Arr("馬ID")
+			);
+
+			// TODO indexの作成
+		}
+
+		public static async Task InsertOrigAsync(this SQLiteControl conn, List<Dictionary<string, string>> racearr)
+		{
+			async Task InsertKettoAsync(string uma)
+			{
+				if (0 == await conn.ExecuteScalarAsync("SELECT COUNT(*) FROM t_orig_k WHERE 馬ID = ?", SQLiteUtil.CreateParameter(DbType.Object, uma)).RunAsync(x => x.GetInt32()))
+				{
+					await foreach (var ketto in NetkeibaGetter.GetKetto(uma))
+					{
+						await conn.InsertAsync("t_orig_k", ketto);
+					}
+				}
+			}
+
+			var first = true;
+
+			foreach (var x in racearr)
+			{
+				if (first)
+				{
+					string ToValue(string s)
+					{
+						switch (s)
+						{
+							case "優勝賞金":
+								return racearr.Max(x => x["賞金"].GetDouble()).Str();
+							case "頭数":
+								return racearr.Count.Str();
+							default:
+								return x[s];
+						}
+					}
+					first = false;
+					await conn.InsertAsync("t_orig_h", col_orig_h.ToDictionary(s => s, s => ToValue(s)));
+				}
+				await conn.InsertAsync("t_orig_d", col_orig_d.ToDictionary(s => s, s => x[s]));
+				await InsertKettoAsync(x["馬ID"]);
+			}
+		}
+
+		public static async Task<bool> ExistsOrigAsync(this SQLiteControl conn, string raceid)
+		{
+			var cnt = await conn.ExecuteScalarAsync(
+				"SELECT COUNT(*) FROM t_orig_h WHERE ﾚｰｽID = ?",
+				SQLiteUtil.CreateParameter(DbType.String, raceid)
+			).RunAsync(x => x.GetInt32());
+			return 0 < cnt;
 		}
 	}
 }
